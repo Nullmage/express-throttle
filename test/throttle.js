70668935fa953ba43a5b614a66b44057cb7ba80f
GlurG <martin.tberger@gmail.com>
2016-07-06 23:43:14 +0200

    'burst' no longer a mandatory option (burst, rate) constructor removed minor performance improvement refactored and improved tests

--- test/throttle.js
+++ test/throttle.js
@@ -11,227 +11,181 @@ function close_to(value, target, delta = 0.001) {
 	return Math.abs(value - target) < delta;
 }
 
+function create_app() {
+	var app = express();
+
+	app.get("/", throttle.apply(null, arguments), function(req, res) {
+		res.status(200).json(req.connection.remoteAddress);
+	});
+
+	return app;
+}
+
 test("fail to init...", t => {
 	t.test("...without options", st => {
 		st.throws(throttle, new Error);
 		st.end();
 	});
 
-	t.test("...with options not being an object", st => {
-		st.throws(() => throttle("options"), new Error);
+	t.test("...with first argument not being a string or object", st => {
+		st.throws(() => throttle(5), new Error);
 		st.end();
 	});
 
-	t.test("...with 'burst' option not being a number", st => {
-		st.throws(() => throttle("5"), new Error);
-		st.end();
-	});
-
-	t.test("...with 'rate' option not being a string", st => {
-		st.throws(() => throttle(5, 10), new Error);
-		st.end();
-	});
-
-	t.test("...without providing 'burst' option", st => {
-		st.throws(() => throttle({ "rate": "1/s" }), new Error);
-		st.end();
-	});
-
-	t.test("...without providing 'rate' option", st => {
-		st.throws(() => throttle({ "burst": 5 }), new Error);
-		st.end();
-	});
-
-	t.test("...with 'rate' not being in correct format", st => {
-		st.throws(() => throttle(5, "x/hour"), new Error);
+	t.test("...with empty option object", st => {
+		st.throws(() => throttle({}), new Error);
 		st.end();
 	});
 
 	t.test("...with 'key' not being a function", st => {
-		st.throws(() => throttle({ "burst": 5, "rate": "1/s", "key": 1 }), new Error);
+		st.throws(() => throttle({ "rate": "1/s", "burst": 5, "key": 1 }), new Error);
 		st.end();
 	});
 
 	t.test("...with 'cost' not being a number or function", st => {
-		st.throws(() => throttle({ "burst": 5, "rate": "1/s", "cost": "5" }), new Error);
+		st.throws(() => throttle({ "rate": "1/s", "burst": 5, "cost": "5" }), new Error);
 		st.end();
 	});
 
 	t.test("...with 'on_throttled' not being a function", st => {
-		st.throws(() => throttle({ "burst": 5, "rate": "1/s", "on_throttled": "test" }), new Error);
+		st.throws(() => throttle({ "rate": "1/s", "burst": 5, "on_throttled": "test" }), new Error);
 		st.end();
 	});
 });
 
-test("successfully init", t => {
-	t.doesNotThrow(() => throttle({
-		"burst": 5,
-		"rate": "1/s",
-		"key": () => true,
-		"cost": () => true,
-		"on_throttled": () => true
-	}));
-	
-	t.end();
-});
-
-test("make one request", t => {
-	var app = express();
-
-	app.get("/", throttle(5, "1/s"), function(req, res) {
-		res.status(200).end();
+test("init with...", t => {
+	t.test("...rate", st => {
+		st.doesNotThrow(() => throttle("1/s"));
+		st.end();
 	});
 
-	request(app).get("/").end((err, res) => {
-		t.equal(res.status, 200);
-		t.end();
+	t.test("...options object", st => {
+		st.doesNotThrow(() => throttle({
+			"rate": "1/s",
+			"burst": 5,
+			"key": () => true,
+			"cost": () => true,
+			"on_throttled": () => true
+		}));
+		
+		st.end();
+	});
+});
+
+test("passthrough request...", t => {
+	function verify(st) {
+		return function(err, res) {
+			st.equal(res.status, 200);
+			st.end();
+		}
+	}
+
+	t.test("...rate (integer)", st => {
+		var app = create_app("1/s");
+		request(app).get("/").end(verify(st));
+	});
+
+	t.test("...rate (decimal)", st => {
+		var app = create_app("1.0/s");
+		request(app).get("/").end(verify(st));
+	});
+
+	t.test("...delayed", st => {
+		var app = create_app("1/s");
+		request(app).get("/").end(() => true);
+		setTimeout(() => {
+			request(app).get("/").end(verify(st));
+		}, 1000);
+	});
+});
+
+test("throttle request...", t => {
+	function verify(st) {
+		return function(err, res) {
+			st.equal(res.status, 429);
+			st.end();
+		}
+	}
+
+	t.test("...rate (integer)", st => {
+		var app = create_app("1/s");
+		request(app).get("/").end(() => true);
+		request(app).get("/").end(verify(st));
+	});
+
+	t.test("...rate (decimal)", st => {
+		var app = create_app("1.0/s");
+		request(app).get("/").end(() => true);
+		request(app).get("/").end(verify(st));
+	});
+
+	t.test("...delayed", st => {
+		var app = create_app("1/s");
+		request(app).get("/").end(() => true);
+		setTimeout(() => {
+			request(app).get("/").end(verify(st));
+		}, 900);
 	});
 });
 
 test("custom store", t => {
-	var app = express();
 	var store = new MemoryStore();
-
-	app.get("/", throttle({
-		"burst": 5,
-		"rate": "1/s",
-		"store": store
-	}), function(req, res) {
-		res.status(200).json(req.connection.remoteAddress);
-	});
+	var app = create_app({ "rate": "1/s", "store": store })
 
 	request(app).get("/").end((err, res) => {
 		t.equal(res.status, 200);
 		store.get(res.body, (err, entry) => {
-			t.assert(close_to(entry.tokens, 4));
+			t.ok(entry);
 			t.end();
 		});
 	});
 });
 
 test("respect x-forwarded-for header", t => {
-	var app = express();
 	var store = new MemoryStore();
-
-	app.get("/", throttle({
-		"burst": 5,
-		"rate": "1/s",
-		"store": store
-	}), function(req, res) {
-		res.status(200).json();
-	});
-
 	var proxy_ip = "123.123.123.123";
+	var app = create_app({ "rate": "1/s", "store": store });
 
-	request(app).get("/")
-	.set("x-forwarded-for", proxy_ip)
-	.end((err, res) => {
+	request(app).get("/").set("x-forwarded-for", proxy_ip).end((err, res) => {
 		t.equal(res.status, 200);
 		store.get(proxy_ip, (err, entry) => {
-			t.assert(close_to(entry.tokens, 4));
+			t.ok(entry);
 			t.end();
 		});
 	});
 });
 
 test("custom key function", t => {
-	var app = express();
 	var store = new MemoryStore();
 	var custom_key = "custom_key";
-
-	app.get("/", throttle({
-		"burst": 5,
+	var app = create_app({
 		"rate": "1/s",
 		"store": store,
-		"key": function() {
-			return custom_key;
-		}
-	}), function(req, res) {
-		res.status(200).end();
+		"key": function() { return custom_key }
 	});
 
 	request(app).get("/").end((err, res) => {
 		t.equal(res.status, 200);
 		store.get(custom_key, (err, entry) => {
-			t.assert(close_to(entry.tokens, 4));
+			t.ok(entry);
 			t.end();
 		});
 	});
 });
 
-test("throttling", t => {
-	var app = express();
+test("custom cost value", t => {
 	var store = new MemoryStore();
-
-	app.get("/", throttle({
-		"burst": 1,
+	var app = create_app({
 		"rate": "1/s",
-		"store": store
-	}), function(req, res) {
-		res.status(200).json(req.connection.remoteAddress);
-	});
-
-	request(app).get("/").end(() => true);
-	request(app).get("/").end((err, res) => {
-		t.equal(res.status, 429);
-	});
-
-	setTimeout(() => {
-		request(app).get("/").end((err, res) => {
-			store.get(res.body, (err, entry) => {
-				t.equal(res.status, 200);
-				t.assert(close_to(entry.tokens, 0));
-				t.end();
-			});
-		});
-	}, 1000);
-});
-
-test("throttling (decimal rate)", t => {
-	var app = express();
-	var store = new MemoryStore();
-
-	app.get("/", throttle({
-		"burst": 1,
-		"rate": "2.5/s",
-		"store": store
-	}), function(req, res) {
-		res.status(200).json(req.connection.remoteAddress);
-	});
-
-	request(app).get("/").end(() => true);
-	request(app).get("/").end((err, res) => {
-		t.equal(res.status, 429);
-	});
-
-	setTimeout(() => {
-		request(app).get("/").end((err, res) => {
-			store.get(res.body, (err, entry) => {
-				t.equal(res.status, 200);
-				t.assert(close_to(entry.tokens, 0));
-				t.end();
-			});
-		});
-	}, 400);
-});
-
-test("custom cost", t => {
-	var app = express();
-	var store = new MemoryStore();
-
-	app.get("/", throttle({
 		"burst": 5,
-		"rate": "1/s",
 		"store": store,
-		"cost": 5
-	}), function(req, res) {
-		res.status(200).json(req.connection.remoteAddress);
+		"cost": 3
 	});
 
 	request(app).get("/").end((err, res) => {
 		store.get(res.body, (err, entry) => {
 			t.equal(res.status, 200);
-			t.assert(close_to(entry.tokens, 0));
+			t.assert(close_to(entry.tokens, 2));
 			t.end();
 		});
 	});
@@ -249,7 +203,7 @@ test("custom cost function", t => {
 			if (req.params.admin == "yes") {
 				return 0;
 			} else {
-				return 5;
+				return 3;
 			}
 		}
 	}), function(req, res) {
@@ -264,7 +218,7 @@ test("custom cost function", t => {
 			request(app).get("/no").end((err, res) => {
 				store.get(res.body, (err, entry) => {
 					t.equal(res.status, 200);
-					t.assert(close_to(entry.tokens, 0));
+					t.assert(close_to(entry.tokens, 2));
 					t.end();
 				});
 			});
@@ -273,20 +227,16 @@ test("custom cost function", t => {
 });
 
 test("custom on_throttled function", t => {
-	var app = express();
-	app.get("/", throttle({
-		"burst": 1,
+	var app = create_app({
 		"rate": "1/s",
 		"on_throttled": function(req, res) {
-			res.status(429).json("slow down!");
+			res.status(503).json("slow down!");
 		}
-	}), function(req, res) {
-		res.status(200).end();
 	});
 
 	request(app).get("/").end(() => true);
 	request(app).get("/").end((err, res) => {
-		t.equal(res.status, 429);
+		t.equal(res.status, 503);
 		t.equal(res.body, "slow down!");
 		t.end();
 	});
